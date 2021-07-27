9a56c96a21ac9bbde499dbc37f493138ba860c2c
seujung <digit822gmail.com>
2021-07-27 16:11:37 +0900

    bug fixed

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,4 @@
-python train.py --gradient_clip_val 1.0
+python train.py --gradient_clip_val 1.0 \
                 --max_epochs 50 \
                 --default_root_dir logs \
                 --gpus 1 \
